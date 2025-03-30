52432c620d24ef79263df1407f90a5df2c2de2ff
Ahmed Loubiri <ahmed.loubiri@insat.ucar.tn>
2025-03-30 02:33:56 +0100

    Food init

--- Snake.py
+++ Snake.py
@@ -13,7 +13,12 @@ BACKGROUND_COLOR = "#000000"
 class Snake:
     pass
 class Food:
-    pass
+    def __init__(self):
+        x = random.randint(0, int((GAME_WIDTH / SPACE_SIZE) - 1)) * SPACE_SIZE
+        y = random.randint(0, int((GAME_HEIGHT / SPACE_SIZE) - 1)) * SPACE_SIZE
+        self.coordinates = [x, y]
+        canvas.create_oval(x, y, x+SPACE_SIZE, y+SPACE_SIZE, fill = FOOD_COLOR, tag = "food")    
+
 
 def nextTurn():
     pass
@@ -52,5 +57,7 @@ x = int((screen_width / 2) - (window_width / 2))
 y = int((screen_height / 2) - (window_height / 2))
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+food = Food()
 
 window.mainloop()
